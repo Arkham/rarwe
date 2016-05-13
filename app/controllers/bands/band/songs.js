f3e2162f7929186857e10e76790989b116d96b8a
Ju Liu <ju.liu@welaika.com>
2016-05-13 10:35:56 +0100

    Add new bands and new songs

--- app/controllers/bands/band/songs.js
+++ app/controllers/bands/band/songs.js
@@ -1,12 +1,23 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  title: '',
+  songCreationStarted: false,
+
+  isAddingButtonDisabled: Ember.computed.empty('title'),
+  noSongs: Ember.computed.empty('model.songs'),
+  canCreateSong: Ember.computed.or('songCreationStarted', 'model.songs.length'),
+
   actions: {
     updateRating: function(params) {
       var song = params.item,
         rating = params.rating;
 
       song.set('rating', rating);
+    },
+
+    enableSongCreation: function() {
+      this.set('songCreationStarted', true);
     }
   }
 });
